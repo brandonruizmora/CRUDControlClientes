a99f0eea5b895fb5e553dda88f3108ab54cbdcc7
brandonruizmora <brandon_ruizmor@outlook.es>
2022-07-21 15:13:27 -0500

    first commit

---
+++ src/test/java/mx/com/crud/ControlClientes/ControlClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.crud.ControlClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
