af7648a1af71873335b34356b0782537a4853050
brandonruizmora <brandon_ruizmor@outlook.es>
2022-07-21 18:52:34 -0500

    Added new view modificar and modified controllers to add new persona

--- src/main/java/mx/com/crud/ControlClientes/web/ControladorInicio.java
+++ src/main/java/mx/com/crud/ControlClientes/web/ControladorInicio.java
@@ -2,11 +2,13 @@ package mx.com.crud.ControlClientes.web;
 
 import lombok.extern.slf4j.Slf4j;
 import lombok.var;
+import mx.com.crud.ControlClientes.domain.Persona;
 import mx.com.crud.ControlClientes.service.IPersonaServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
@@ -21,6 +23,17 @@ public class ControladorInicio {
         var personas = iPersonaServicio.listarPersonas();
         model.addAttribute("personas", personas);
         return "index";
+    }
+
+    @GetMapping("/agregar")
+    public String agregar(Persona persona) {
+        return "modificar";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(Persona persona) {
+        iPersonaServicio.guardar(persona);
+        return "redirect:/";
     }
 
 }
